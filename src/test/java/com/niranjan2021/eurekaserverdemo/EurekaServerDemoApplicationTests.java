85845b063a8ca3aa68344a4f4b4d827f46e72b63
niranjan-l <raoniranjan007@gmail.com>
2021-06-01 20:36:37 +0530

    Initial commit

---
+++ src/test/java/com/niranjan2021/eurekaserverdemo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niranjan2021.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+/*
+	@Test
+	void contextLoads() {
+	}*/
+
+}
